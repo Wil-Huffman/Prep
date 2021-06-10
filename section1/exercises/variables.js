b3a6c1f5149e201e9905b130158289eb8dbfeeef
Wil Huffman <me@wil-huffman.io>
2021-06-10 17:44:15 -0500

    Initial Commit

---
+++ section1/exercises/variables.js
@@ -0,0 +1,55 @@
+/*
+In the exercises below, write your own code where indicated
+to achieve the desired result.
+
+A couple examples that are already completed. Your task is to complete
+any remaining prompt.
+
+When navigated to the root of this project directory, you should be able to run this
+file from your terminal with the command `node section1/exercises/variables.js`
+*/
+
+
+// EXAMPLE: Write code below to assign Harry Potter's name to a variable and
+// log that variable to the console:
+var name = "Harry Potter";
+console.log(name);
+
+// EXAMPLE: Write code below to add 2 to the variable `students` and
+// log the result:
+var enrolledStudents = 22;
+console.log(enrolledStudents);
+
+// YOU DO:
+// Write code below to save the string 'Harry Potter must not return to Hogwarts!'
+// log that variable to the console.
+
+
+// YOU DO:
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+  // Store the appropriate data types in each.
+// log all three variables to the console.
+
+// IN WORDS:
+// How did you decide to use the data type you did for each of the three variables above? 
+  // Explain.
+
+
+// YOU DO:
+// Re-assign the values to the three variables from the previous challenge to different 
+  // values (but same data type).
+// log all three variables to the console.
+
+
+// YOU DO:
+// Using the variables below, log the total number of snacks to the console:
+var healthySnacks = 6;
+var junkFoodSnacks = 8;
+
+
+//-------------------
+// FINAL CHECK
+//-------------------
+
+// Did you run this file in your terminal to make sure everything printed out to the console 
+// as you would expect? 
---
+++ section3/reflection.md
@@ -0,0 +1,15 @@
+## Section 3 Reflection
+
+1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
+
+1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+1. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
+
+1. In your own words, explain what an Array method is.
+
+1. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
+
+1. If you had to teach a 5 year old about loops, how would you explain it?
+
+1. What questions do you still have about Arrays and loops?
